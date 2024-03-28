94918431bb07d23bbfe6969f33f5be45b4c92291
myname <sheikhjunaid732000@gmail.com>
2024-03-28 15:43:47 +0530

    al components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
-import './App.css'
+import Header from './components/Header/Header'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-green-500 p-4'>React Router</h1>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+    <Footer />
     </>
   )
 }
